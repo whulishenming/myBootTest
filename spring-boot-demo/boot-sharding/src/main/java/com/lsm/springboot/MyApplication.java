3624dce675fe47cfdbad533651d7932495d67bce
lishenming <lishenming8@126.com>
2017-08-16 00:10:30 +0800

    sharding 文档

--- spring-boot-demo/boot-sharding/src/main/java/com/lsm/springboot/MyApplication.java
+++ spring-boot-demo/boot-sharding/src/main/java/com/lsm/springboot/MyApplication.java
@@ -5,6 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by lishenming on 2017/4/18.
+ * https://dangdangdotcom.github.io/sharding-jdbc/00-overview/
+ * sharding 支持
+ *  1. INSERT
+ *  2. UPDATE
+ *  3. DELETE
+ *  4. SELECT主语句
+        SELECT select_expr [, select_expr ...] FROM table_reference [, table_reference ...]
+            [WHERE where_condition]
+            [GROUP BY {col_name | position} [ASC | DESC]]
+            [ORDER BY {col_name | position} [ASC | DESC], ...]
+            [LIMIT {[offset,] row_count | row_count OFFSET offset}]
  */
 @SpringBootApplication
 public class MyApplication {
