e1daf9ab7799c498405c27986b353fcc5fdd11db
lishenming <lishenming8@126.com>
2017-09-12 23:30:06 +0800

    boot-configuration

--- spring-boot-demo/boot-mongo-log4j2/src/main/java/com/lsm/springboot/MyApplication.java
+++ spring-boot-demo/boot-mongo-log4j2/src/main/java/com/lsm/springboot/MyApplication.java
@@ -2,11 +2,13 @@ package com.lsm.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 /**
  * Created by lishenming on 2017/4/18.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class,MongoDataAutoConfiguration.class})
 public class MyApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MyApplication.class, args);
